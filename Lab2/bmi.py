73eb65dde724360b90b9394b784d2fcd18aefc0f
vishal jn <vishaljn.25@ichat.sp.edu.sg>
2026-04-29 10:22:19 +0800

    new file is bmi.py

---
+++ Lab2/bmi.py
@@ -0,0 +1,14 @@
+
+
+def calculate_bmi(height, weight,):
+    print("Height="+str(height))
+    print("Weight="+str(weight))
+    bmi=weight/(height*height)
+    print("bmi="+str(bmi))
+    if(bmi<18.5):
+        print("Weight classification: Under Weight")
+    elif(bmi>=18.5) & (bmi<=25.0):
+        print("Weight classification: Normal Weight")
+    elif(bmi>=25):
+        print("Weight classification: Over weight")
+calculate_bmi(weight=53,height=1.73)
