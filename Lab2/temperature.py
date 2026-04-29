f6b3050aac32d0745e36179976d21baad079af87
vishal jn <vishaljn.25@ichat.sp.edu.sg>
2026-04-29 11:13:35 +0800

    this calculate average temp

---
+++ Lab2/temperature.py
@@ -0,0 +1,7 @@
+def calc_average_temperature(temp1, temp2, temp3):
+    print("temp1="+str(temp1))
+    print("temp2="+str(temp2))
+    print("temp3="+str(temp3))
+    average_temp =(temp1+temp2+temp3)/3
+    print("The average temperature:"+str(average_temp))
+calc_average_temperature(temp1=20.4,temp2=50.6,temp3=30.5)
