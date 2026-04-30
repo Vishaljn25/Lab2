a658647c0f4b0d1442835239ffba7251acc108b8
vishal jn <vishaljn.25@ichat.sp.edu.sg>
2026-04-30 16:44:36 +0800

    this is a another practise

---
+++ max_min_temp.py
@@ -0,0 +1,5 @@
+def calc_min_max_temperature(temp_list):
+ print("The maximum temperature:"+str(max(temp_list)))
+ print("The minimum temperature:"+str(min(temp_list)))
+
+calc_min_max_temperature(temp_list =[20,39,329,576,23])
