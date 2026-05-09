6b7a2e2a07f34cf0bbae8136bc99f3cadb1941a1
vishal jn <vishaljn.25@ichat.sp.edu.sg>
2026-05-09 19:23:00 +0800

    this is the neewer ver

--- bmi.py
+++ bmi.py
@@ -1,15 +1,16 @@
 
 
-def calculate_bmi():
-    height=float(input("Enter Height :"))
-    weight=float(input("Enter Weight :"))
+
+def calculate_bmi(weight, height):
+    print("Entered Height :" + str(height))
+    print("Entered Weight :"+ str(weight))
     bmi=weight/(height**2)
     print("bmi="+str(bmi))
     if(bmi<18.5):
         x=-1
-    elif(bmi>=18.5) & (bmi<=25.0):
+    elif(bmi>=18.5) and (bmi<=25.0):
         x=0
-    elif(bmi>=25):
+    else:
         x=1
-    return x       
-calculate_bmi()
+    return x 
+calculate_bmi(70,1.74)
